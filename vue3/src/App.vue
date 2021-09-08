49cf9e99622bc0ef1fb8f2a24fe9e8e137ee8e1e
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2021-09-08 14:44:55 -0600

    trabajando con Vue components

---
+++ vue3/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>Peliculas</h2>
+  <Form />
+</template>
+
+<script>
+import Form from "./components/Form.vue";
+export default {
+  name: "App",
+  components: {
+    Form: Form,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3/src/components/Form.vue
@@ -0,0 +1,47 @@
+<template>
+  <form v-for="movie in movies">
+    <h3>{{ movie.name }}</h3>
+
+    <button
+      type="button"
+      v-on:click="movie.quantity -= 1"
+      :disabled="movie.quantity <= 0"
+    >
+      -
+    </button>
+    {{ movie.quantity }}
+    <button
+      type="button"
+      v-on:click="movie.quantity += 1"
+      :disabled="movie.quantity >= movie.available"
+    >
+      +
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Form",
+  data() {
+    return {
+      movies: [
+        {
+          name: "Avengers",
+          available: 3,
+          quantity: 0,
+        },
+        {
+          name: "Terminator",
+          available: 5,
+          quantity: 0,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
